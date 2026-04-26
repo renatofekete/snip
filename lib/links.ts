102fd07d585077146d3c1a263b47a628d2660ab1
Renato <renatofekete.91@gmail.com>
2026-04-26 14:08:57 +0200

    get link by short code

---
+++ lib/links.ts
@@ -0,0 +1,11 @@
+import client from "@/lib/supabase";
+
+export const getLinkByShortCode = async (shortCode: string) => {
+  const { data, error } = await client
+    .from("links")
+    .select("original_url")
+    .eq("short_code", shortCode)
+    .single();
+
+  return data?.original_url ?? null;
+};
